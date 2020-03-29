d97a777e3e2326d1e661eb33f2a5b01dba54a10a
Gabriel Madruga <gabriel.madru@gmail.com>
2020-03-28 22:07:09 -0300

    Fixes 0. bug

--- MacroCounter/Helpers/UITextFieldExtensions.swift
+++ MacroCounter/Helpers/UITextFieldExtensions.swift
@@ -14,7 +14,11 @@ extension UITextField {
         guard var text = self.text, text.count > 0 else {
             return nil
         }
-        if text.count > 1 && text.first == "0" {
+        if text == "." {
+            self.text = "0."
+            text = "0."
+        }
+        if text.count == 2 && text.first == "0" && text != "0." {
             self.text = String(text.dropFirst())
         }
         let splits = Array(text.split(separator: ".", maxSplits: 2, omittingEmptySubsequences: false))
